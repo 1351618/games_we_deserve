b6ffc782d1c3657b9108f86079363e8f4bd2dced
1351618 <1351618@gmail.com>
2023-08-31 18:53:14 +0300

    style

--- modules/record_player.js
+++ modules/record_player.js
@@ -1,7 +1,7 @@
 // todo record_player.js      аудио проигрыватель
 
 const SoundsTimerMP3 = document.getElementById("sounds-timer-mp3");
-
+const TurnOffSound = document.getElementById("turn_off_sound");
 playButton.addEventListener("click", () => {
     SoundsTimerMP3.play();
 });
